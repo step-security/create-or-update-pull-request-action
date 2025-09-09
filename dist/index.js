11909b17654caf5972be587c069ef9b5d9a5e1b8
Raj-StepSecurity <rajkumar@stepsecurity.io>
2025-09-09 05:08:29 +0000

    Merge 6eb99b55f864c06da7604b6a09d9b92082d9c88c into 744680b1045dd400e355ca125f4d658dcaaf63de

--- dist/index.js
+++ dist/index.js
@@ -39109,7 +39109,7 @@ async function validateSubscription() {
   try {
     await lib_axios.get(API_URL, {timeout: 3000});
   } catch (error) {
-    if (axios_isAxiosError(error) && error.response) {
+    if (axios_isAxiosError(error) && error.response?.status === 403) {
       core.error(
         'Subscription is not valid. Reach out to support@stepsecurity.io'
       );
