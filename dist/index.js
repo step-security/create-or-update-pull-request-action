a857a914c6fded381d90e569b4ed79ec16166ea3
Raj-StepSecurity <rajkumar@stepsecurity.io>
2025-09-12 05:34:17 +0000

    Merge b0cfe1946f5f0e2778e35141bff7f2c213e5350f into 73d8256e5ebf93a2acdb68557bd1a98ea57e52cc

--- dist/index.js
+++ dist/index.js
@@ -38903,6 +38903,7 @@ async function main() {
       const q = `head:${inputs.branch} type:pr is:open repo:${process.env.GITHUB_REPOSITORY}`;
       const { data } = await octokit.request("GET /search/issues", {
         q,
+        advanced_search: true,
       });
 
       if (data.total_count > 0) {
